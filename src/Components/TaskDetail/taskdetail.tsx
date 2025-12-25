f7ebb434f77773138e0332dc0bf63e793a2c455c
Konstantin <123>
2025-12-25 21:32:51 +0900

    next

--- src/Components/TaskDetail/taskdetail.tsx
+++ src/Components/TaskDetail/taskdetail.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useRef, useState, useEffect } from "react";
+import React, { FunctionComponent, useRef, useState, useEffect, useCallback } from "react";
 import  style  from "./style.module.scss"
 import { useParams, useLocation } from 'react-router-dom'
 import { getTaskList } from '../LocalStorage/localstorage'
@@ -8,42 +8,53 @@ const TaskDetail: FunctionComponent = (): React.JSX.Element => {
     const id = useParams()
     const lokation = useLocation()
     const refDescription = useRef(null)
-
     const refFormDescription = useRef(null)
-    let tasklist = getTaskList(lokation.state.listName)
     const [title, setTitel] = useState<string>("")
     const [description, setDescription] = useState<string>("")
     const [valueForm, setValueForm] = useState<string>("")
+    let tasklist = getTaskList(lokation.state.listName)
+    
 
-    useEffect(()=>{
-        const form:any = refFormDescription.current
+      
         for(let task of tasklist) {      
             if (task.id === Number(id.taskId)) {
-                setTitel(task.title)
+                setTitel(task.title)           
                 setDescription(task.description)
-                form.value = task.description
-            }
-        }},)
+                setValueForm(task.description)
+                console.log("1")
+            }}
+
+
 
     useEffect(()=>{
+
         const text:any = refDescription.current
         const input:any = refFormDescription.current
 
         function clickText() {
             text.style.display = "none"
             input.style.display = "flex"
+            input.focus()
         }
         function clickEnter(e:any) {
             if(e.key ==="Enter"){
             text.style.display = "flex"
             input.style.display = "none"
             setDescription(input.value)
-            console.log(input.value)
+            }
+        }
+        function clickEscape(e:any) {
+            if(e.key ==="Escape"){
+            //text.style.display = "flex"
+            //input.style.display = "none"
+            console.log(description)
+            //setValueForm(text.value)
             }
         }
 
         text.addEventListener("click", clickText)
         input.addEventListener("keydown", clickEnter)
+        input.addEventListener("keydown", clickEscape)
     },[])
 
     
@@ -53,7 +64,7 @@ const TaskDetail: FunctionComponent = (): React.JSX.Element => {
         <div className={style.taskDetail}>
             <span className={style.taskDetail__title}>{title}</span>
             <span ref={refDescription} className={style.taskDetail__description}>{description}</span>
-            <input ref={refFormDescription} className={style.input} type="text"  />
+            <textarea ref={refFormDescription} value={valueForm} onChange={event => setValueForm(event.target.value)} className={style.input} />
             
         </div>
     )
